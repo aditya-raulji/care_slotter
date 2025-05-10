666f673af582eccacf54568be948c7783809dd11
aditya-raulji <-email@example.com>
2025-05-10 17:13:08 +0530

    Changed vite file

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port:5173 ,
+  server: {
+    port: 5173,
     allowedHosts: [
-        '0362-2402-3a80-4662-2fc7-ddf8-76c1-7285-b2d4.ngrok-free.app'
+      '0362-2402-3a80-4662-2fc7-ddf8-76c1-7285-b2d4.ngrok-free.app'
     ]
-}
-
-  
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+    allowedHosts: [
+      'care-slotter-doctor-appointment.onrender.com' // <-- Render production domain
+    ]
+  }
 })
